b6f47f7017223752b45c6860a9cf4779f7ad63c9
Shruti Gupta <shruti.gupta2@mphasis.com>
2026-03-06 01:46:05 +0530

    Initial project setup

---
+++ src/test/java/com/Transaction/AegisCapital/AegisCapitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Transaction.AegisCapital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisCapitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
