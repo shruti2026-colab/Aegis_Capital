6cb7a1a4df93a8b60fdb877083a4b25aa54e59cf
Anill2930 <p.anil@mphasis.com>
2026-03-06 17:36:12 +0530

    Updated Account Entity

--- src/main/java/com/aegiscapital/entity/Account.java
+++ src/main/java/com/aegiscapital/entity/Account.java
@@ -1,4 +1,34 @@
 package com.aegiscapital.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name ="accounts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal balance;  // to store amount in account, check balance
+
+
+    // optimistic locking to prevent concurrent update issues
+    @Version
+    private Long version;
+
+
+    // if may be more than one account belong to one user
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }
