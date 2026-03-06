a76cbcb32309029bbfe61a358261dbb8cacb2a91
Richa Dubey <richa.dubey@mphasis.com>
2026-03-06 13:51:50 +0530

    Fix: package name to com.aegiscapital; add datasource exclusion temporarily

---
+++ src/main/java/com/aegiscapital/AegisCapitalApplication.java
@@ -0,0 +1,16 @@
+package com.aegiscapital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+
+
+
+public class AegisCapitalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AegisCapitalApplication.class, args);
+    }
+}
--- src/test/java/com/aegiscapital/AegisCapitalApplicationTests.java
+++ src/test/java/com/aegiscapital/AegisCapitalApplicationTests.java
@@ -1,4 +1,4 @@
-package com.Transaction.AegisCapital;
+package com.aegiscapital;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
