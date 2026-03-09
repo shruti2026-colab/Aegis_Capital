7510501e7318834e18acc6cd55278a9dabcbb651
Anill2930 <p.anil@mphasis.com>
2026-03-09 13:31:44 +0530

    Created Account,User, Transaction repositories

--- src/main/java/com/aegiscapital/respository/UserRepository.java
+++ src/main/java/com/aegiscapital/respository/UserRepository.java
@@ -8,7 +8,10 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    // helps to find user details by email
     Optional<User> findByEmail(String email);
 
+
+    // checks if user exists with the email we looking for
     boolean existsByEmail(String email);
 }
