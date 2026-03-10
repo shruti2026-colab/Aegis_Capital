2ceec3942fa352c96149b146427dc911127733d9
Richa Dubey <richa.dubey@mphasis.com>
2026-03-10 13:07:14 +0530

    AuthController Added,Register and Login Api working

---
+++ src/main/java/com/aegiscapital/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.aegiscapital.controller;
+
+
+
+import com.aegiscapital.dto.LoginRequestDTO;
+import com.aegiscapital.dto.RegisterRequestDTO;
+import com.aegiscapital.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequestDTO request) {
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequestDTO request) {
+        return authService.login(request);
+    }
+}
