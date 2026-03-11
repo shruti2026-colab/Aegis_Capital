55ebac09e0215846a159f598f002dbf5c19217cf
Richa Dubey <richa.dubey@mphasis.com>
2026-03-11 14:35:16 +0530

    Implemented GlobalExceptionHandler with handlers for OptimisticLockingFailureException

---
+++ src/main/java/com/aegiscapital/exception/ConcurrentTransactionException.java
@@ -0,0 +1,9 @@
+package com.aegiscapital.exception;
+
+public class ConcurrentTransactionException extends RuntimeException{
+    public ConcurrentTransactionException(String message){
+        super(message);
+    }
+}
+
+
