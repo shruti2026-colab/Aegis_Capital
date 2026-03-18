5f8677e62d9cc0f3733959fa19860401a5f52e14
Richa Dubey <richa.dubey@mphasis.com>
2026-03-18 14:00:58 +0530

    add password encryption for user authentication
    - Integrated BCryptPasswordEncoder
    - Encrypted passwords during user registration
    - Verified login using passwordEncoder.matches()
    - Prevented plain-text password storage

--- src/main/java/com/aegiscapital/service/AuthServiceImpl.java
+++ src/main/java/com/aegiscapital/service/AuthServiceImpl.java
@@ -7,6 +7,7 @@ import com.aegiscapital.dto.RegisterRequestDTO;
 import com.aegiscapital.entity.User;
 import com.aegiscapital.respository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +15,8 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
 
     @Override
     public String register(RegisterRequestDTO request) {
@@ -25,12 +28,16 @@ public class AuthServiceImpl implements AuthService {
         User user = new User();
         user.setName(request.getName());
         user.setEmail(request.getEmail());
-        user.setPassword(request.getPassword());
+
+        //  ENCRYPT PASSWORD
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
 
         userRepository.save(user);
 
         return "User registered successfully!";
     }
+
+
 
     @Override
     public String login(LoginRequestDTO request) {
@@ -42,7 +49,8 @@ public class AuthServiceImpl implements AuthService {
             return "User does not exist!";
         }
 
-        if (!user.getPassword().equals(request.getPassword())) {
+        // PROPER PASSWORD CHECK
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             return "Invalid password!";
         }
 
